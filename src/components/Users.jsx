3bcc4aa74bd535ea428bedfc17de3bef7208a28f
Ali <ali.ozgur@example.com>
2022-10-26 01:47:00 +0300

    Users Componets created

---
+++ src/components/Users.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect,useState } from "react";
+
+const Users = () => {
+const [users, setUsers] = useState([])
+// Çektiğimiz veri gelene kadar kullanıcıya ekranda bilgi vermek için bir state daha kullandım.
+const[loading, setLoading] = useState(true)
+
+// Api çekme işlemlerinde veri ilk kez çekmek için useEffect Hook'unu kullanıyoruz. 
+// Bu çektiğimiz veriyide göstermek için useState Hook'unu kullanıyoruz. 
+    useEffect(()=>{
+        try {
+            fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res)=>res.json())
+            .then((data)=>setUsers(data))
+            .then(()=>setLoading(false))
+            
+            
+        } catch (error) { console.log(error)
+            
+        }
+        
+    },[]);
+
+  return (
+    <div className="users">
+        
+
+        {loading && <div className="load">Loading...</div>}
+        <ul>
+{users.map((user)=>
+
+<li key={user.id}>{user.name} </li>)}
+
+        </ul>
+
+
+
+    </div>
+  )
+}
+
+export default Users
